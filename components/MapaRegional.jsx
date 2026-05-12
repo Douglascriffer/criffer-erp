75024af81ad5a1b9009afcb38aaad2e5a83cfc15
Douglas Bitencourt <douglas.bitencourt@criffer.com.br>
2026-05-12 16:20:41 -0300

    style: increase regional icons by 5px in all dashboards

--- components/MapaRegional.jsx
+++ components/MapaRegional.jsx
@@ -50,8 +50,8 @@ export default function MapaRegional({ stateData = [], officialTotal = 0, darkMo
           <div key={item.name}>
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 6, fontSize: 12 }}>
               <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-                 <div style={{ width: 24, height: 20, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <IconeRegiao regiao={item.name.toUpperCase()} size={20} />
+                 <div style={{ width: 29, height: 25, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <IconeRegiao regiao={item.name.toUpperCase()} size={25} />
                  </div>
                  <span style={{ color: darkMode ? '#ffffff' : '#000000', fontWeight: 700 }}>{item.name}</span>
               </div>
