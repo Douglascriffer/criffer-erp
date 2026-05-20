9c94634aa87aedb79644513befb250f2dfa3055e
Douglas Bitencourt <douglas.bitencourt@criffer.com.br>
2026-05-20 08:50:45 -0300

    style: ajustado enquadramento vertical de Rogislei e Gabriel Ferreira no Modo TV

--- components/TVMode.jsx
+++ components/TVMode.jsx
@@ -46,12 +46,12 @@ const PHOTO_MAP = {
 }
 
 const PHOTO_POSITION_MAP = {
-  'Rogislei Vieira Padilha': 'center 30%',
+  'Rogislei Vieira Padilha': 'center 15%',
   'Gabriel Dias': 'center 30%',
   'Vanessa Ferreira': 'center 30%',
   'Gabriel Medeiros': 'center 15%',
   'Josiane Govoni Lanzarini': 'center 15%',
-  'Gabriel Ferreira dos Santos': 'center 46%'
+  'Gabriel Ferreira dos Santos': 'center 20%'
 }
 
 export default function TVMode({ data, mes = 'all' }) {
